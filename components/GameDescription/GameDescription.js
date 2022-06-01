13dc8f095bf8a204d79fbe14e76dd5feaf247e2e
Benjamin Pallay <benjaminpallay@gmail.com>
2022-06-01 15:51:24 +1000

    Add swipe description to how to play.

--- components/GameDescription/GameDescription.js
+++ components/GameDescription/GameDescription.js
@@ -4,8 +4,9 @@ const GameDescription = () => {
       <strong className="font-bold uppercase text-brown-100">
         How to Play
       </strong>
-      : Use your <strong className="text-brown-100">arrow keys</strong> to move
-      the tiles. Tiles with the same number{" "}
+      : Use your <strong className="text-brown-100">arrow keys</strong> or{" "}
+      <strong className="text-brown-100">swipe</strong> to move the tiles. Tiles
+      with the same number{" "}
       <strong className="text-brown-100">merge into one</strong> when they
       touch. Add them up to reach{" "}
       <strong className="text-brown-100">2048!</strong>
